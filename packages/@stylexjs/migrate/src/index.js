d23d3343a3f5d99b7bfa2a8096b641b033885ab1
ahmedsadid <ahmedsadid17@yahoo.com>
2026-08-11 07:20:01 -0400

    [migrate] Resolve composed theme token values

--- packages/@stylexjs/migrate/src/index.js
+++ packages/@stylexjs/migrate/src/index.js
@@ -154,6 +154,8 @@ export {
   validateThemeDecisionDraft,
 } from './theme/model';
 export { discoverThemeFacts } from './theme/discover';
+export { resolveThemeValue } from './theme/resolve';
+export type { ThemeValueResolution } from './theme/resolve';
 export { emitThemeModule } from './theme/emit';
 export { proposeApprovedThemeFiles } from './theme/rewrite';
 export type {
